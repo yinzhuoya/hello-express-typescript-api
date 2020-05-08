fbb7dab8f90f7f42e52a094c666175eb9bec0bd1
yinzhuoya <yinzhuoya@foxmail.com>
2020-05-09 00:47:40 +0800

    #3 建立 Express 服务器

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('hello world!')
+import express, { Express, Request, Response } from 'express'
+
+const app: Express = express()
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send('hello world')
+})
+
+const port: any = process.env.PORT || 6060
+
+const main = () => {
+  app.listen(port, () => {
+    console.log(`Running on http://localhost:${6060}`)
+  })
+}
+
+main()
